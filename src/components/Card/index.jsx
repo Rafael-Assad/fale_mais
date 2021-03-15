3592fe61ccfaf861134284aa1ed1211c08644b6f
[Rafael Assad] <[rafael.assad1010@gmail.com]>
2021-03-14 22:02:31 -0300

    feat: Card component created

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,8 +1,17 @@
 import { CardContainer } from './style'
 
-const Card = () => {
+const Card = ({plan, range}) => {
   return (
-    <CardContainer></CardContainer>
+    <CardContainer>
+      <h2>Fale+ {plan}</h2>
+      <ul>
+        <li>Os primeiro {plan} minutos da chamada não alteram o valor do plano</li>
+        <li>Pague somente minutos adicionais</li>
+        <li>Fale com qualquer DDD de {range}</li>
+        <li>Vantagens exclusivas do plano Fale+</li>
+      </ul>
+      
+    </CardContainer>
   )
 }
 
--- src/components/Card/style.js
+++ src/components/Card/style.js
@@ -1,5 +1,31 @@
 import styled from 'styled-components'
 
 export const CardContainer = styled.div`
+  width: 20rem;
+  max-width: 90vw;
+  height: 17rem;
+
+  padding: 2.5rem;
+  margin-top: 5rem;
+
+  background-color: #FFF;
+
+  box-shadow: 1px 1px 4px #000 ;
+
+  border-radius: 15px;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+
+  h2{
+    font-size: 2.5rem;
+    color: #484747;
+  }
+
+  li{
+    font-size: 0.9rem;
+  }
 
 `
