4b0027f8a0ae7823f1abf1072601373588ace30c
[Rafael Assad] <[rafael.assad1010@gmail.com]>
2021-03-14 22:01:47 -0300

    feat: footer finished the basic

--- src/components/Footerbar/index.jsx
+++ src/components/Footerbar/index.jsx
@@ -1,14 +1,27 @@
-import { FooterContainer } from './style'
-
+import { FooterContainer } from "./style";
+import Button from "../Button";
 const FooterBar = () => {
   return (
     <FooterContainer>
-    <div className='upperFooter'>
-      mais coisas
-    </div>
-      <div className='lowFooter'>Creditos</div>
-    </FooterContainer>
-  )
-}
+      <div className="upperFooter">
+        <div className="buttonBox">
+          <Button 
+            bgColor ='#F6F6F6'
+            textColor ='#67001f'
+            // clickAction={}
 
-export default FooterBar
+          >Contate-nos</Button>
+          <div>
+            <p className='phone'>Televendas: XXXX-XXXX</p>
+            <p className='schedule'>Todos os dias, das 7h as 22h</p>
+          </div>
+        </div>
+
+        <div className="infoBox">Outras informações</div>
+      </div>
+      <div className="lowFooter">Creditos</div>
+    </FooterContainer>
+  );
+};
+
+export default FooterBar;
--- src/components/Footerbar/style.js
+++ src/components/Footerbar/style.js
@@ -8,9 +8,64 @@ export const FooterContainer = styled.footer`
 
   background-color: #aa0033;
 
+  margin-top: 3.5rem;
+
   display: flex;
   flex-direction: column;
   justify-content: space-between;
+
+  .upperFooter{
+    width: 100vw;
+    max-width: 100%;
+
+    height: 87%;
+    padding: 1.5rem;
+
+    display: flex;
+
+    align-items: center;
+    justify-content: space-between;
+
+
+    .buttonBox{
+      width:50%;
+      height:100%;
+
+      border-right: 1px solid #F6F6F6;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content:space-around;
+      
+      button{
+        width: 9rem;
+        font-weight:900;
+      }
+
+      p{
+        color:#F6F6F6;
+        font-size:0.8rem;
+      }
+      .phone{
+        font-weight: 900;
+        font-size:0.8rem;
+      }
+
+      .schedule{
+        font-weight: 300;
+        font-size:0.7rem;
+
+      }
+
+    }
+
+    .infoBox{
+
+      width:45%;
+      height:100%;
+      border: 1px solid #000;
+    }
+  }
 
   .lowFooter{
     background-color: #67001f;
